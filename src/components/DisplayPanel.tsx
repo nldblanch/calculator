644c3c98f733e35cd80ad9e73679332f6843d851
Nathan <nldblanch@gmail.com>
2024-09-04 14:47:58 +0100

    adjust font sizes for scalability and add scroll

--- src/components/DisplayPanel.tsx
+++ src/components/DisplayPanel.tsx
@@ -7,12 +7,12 @@ export const DisplayPanel = ({ output }: SetPropsType): React.JSX.Element => {
   const displayValue: string =
     output === "Error" ? output : formatDisplay(output);
   let style: string =
-    "text-right w-full font-extralight text-white pr-6";
-  if (output.length <= 6) style += " text-4xl";
-  else if (output.length === 7) style += " text-3xl";
-  else if (output.length === 8) style += " text-2xl";
-  else if (output.length === 9) style += " text-xl";
-  else style += " text-lg"
+    "text-right w-full font-extralight text-white pr-6 overflow-x-scroll";
+  if (output.length <= 6) style += " text-6xl";
+  else if (output.length === 7) style += " text-5xl";
+  else if (output.length === 8) style += " text-4xl";
+  else if (output.length === 9) style += " text-3xl";
+  else style += " text-3xl"
   return (
     <section className="w-full h-full flex flex-col justify-end">
       <p className={style}>{displayValue}</p>
