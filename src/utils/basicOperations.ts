5cb91a4d7a524fd31d49a07ef9dd939f45481ed3
Nathan <nldblanch@gmail.com>
2025-06-26 14:40:37 +0100

    commit changes

--- src/utils/basicOperations.ts
+++ src/utils/basicOperations.ts
@@ -12,24 +12,3 @@ export const multiply = (a: number, b: number): number => {
 export const divide = (a: number, b: number): number => {
   return a / b;
 };
-
-//   const person: Readonly<{ name: string; age: number }> = { name: 'Alice', age: 30 };
-// // person.age = 31; // Error: Cannot assign to 'age' because it is a read-only property
-
-// function printPersonDetails({ name, age }: { name: string; age: number }) {
-//     console.log(`Name: ${name}, Age: ${age}`);
-//   }
-
-// const numbers: Array<number> = [1, 2, 3];
-// const firstNumber: number = numbers[0];
-
-// enum Fruit {
-//     APPLE = 'apple',
-//     BANANA = 'banana',
-//     ORANGE = 'orange',
-//   }
-
-//   interface Person {
-//     name: string;
-//     age: number;
-//   }
